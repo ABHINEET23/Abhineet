ef6f3ce42c2995cef1f2484e27e34e1d100d2e7e
ABHINEET23 <118812880+ABHINEET23@users.noreply.github.com>
2022-12-12 12:57:12 +0530

    Add files via upload
    Collection first part

---
+++ CollectionAssignment1.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+public class AscendingSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 List<Integer> list = Arrays.asList(10,1,4,5, 4, 2, 6, 5, 8);
+	        Collections.sort(list);
+	 
+	        System.out.println(list);
+	}
+
+}
